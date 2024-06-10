a38813f3d162438d11eccae4f4681e2cf5ab7562
tcrouzet <tc@tcrouzet.com>
2024-06-10 10:36:38 +0000

    sync 2024-06-10 10:36:38

--- git.sh
+++ git.sh
@@ -5,3 +5,5 @@ git add .
 git commit -m "sync $current_date"
 git push -u origin main
 
+rsync -avz -e "ssh -i /Users/thierrycrouzet/Documents/python/aws/GeogramZefal.pem" ec2-user@ec2-35-180-41-206.eu-west-3.compute.amazonaws.com://var/www/html/geogram /Users/thierrycrouzet/Documents/python/geogramZefal
+
