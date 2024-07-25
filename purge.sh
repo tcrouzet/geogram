0f2a4755c2439ecedcfd9cbc94694b8ba7a3f83e
tcrouzet <tc@tcrouzet.com>
2024-07-25 08:41:39 +0000

    sync 2024-07-25 08:41:39

--- purge.sh
+++ purge.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 #chmod +x purge.sh
-sudo truncate -s 0 /var/www/html/geogram/logs/robot.log
-sudo rm -f /var/www/html/geogram/logs/*.txt
+sudo truncate -s 0 logs/robot.log
+sudo rm -f logs/*.txt
 
+# Find and sort the _backup_ files, then delete all but the latest one
+backup_files=$(ls -1 _backup_*.sql | sort)
+latest_backup=$(echo "$backup_files" | tail -n 1)
+for file in $backup_files; do
+    if [ "$file" != "$latest_backup" ]; then
+        sudo rm -f "$file"
+    fi
+done
