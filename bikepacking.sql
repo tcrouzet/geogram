72461fb53df45846f441affb720ecb6647fd966a
tcrouzet <tc@tcrouzet.com>
2024-08-22 16:15:08 +0000

    sync 2024-08-22 16:15:08

--- bikepacking.sql
+++ bikepacking.sql
@@ -85,7 +85,7 @@ CREATE TABLE `logs` (
   `dev` int(11) NOT NULL,
   `comment` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6169 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=6359 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -111,4 +111,4 @@ CREATE TABLE `options` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-18 19:02:49
+-- Dump completed on 2024-08-22  9:21:37
