f3b05876501fe985977d1b435902cbecf33948e8
tcrouzet <tc@tcrouzet.com>
2024-12-19 15:21:33 +0000

    sync 2024-12-19 15:21:33

--- git.sh
+++ git.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #chmod +x git.sh
+#git config --global credential.helper store
+
 current_date=$(date +"%Y-%m-%d %H:%M:%S")
 git add .
 git commit -m "sync $current_date"
 git push -u origin main
-
